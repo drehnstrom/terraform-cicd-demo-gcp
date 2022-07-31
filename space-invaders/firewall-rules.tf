80bf611fa144101fb800aaf5b0d01c8354cc3c6d
drehnstrom <drehnstrom@yahoo.com>
2022-07-31 11:34:41 -0400

    Update firewall-rules.tf

--- space-invaders/firewall-rules.tf
+++ space-invaders/firewall-rules.tf
@@ -1,6 +1,6 @@
 # allow http
 resource "google_compute_firewall" "allow-http" {
-  count   = var.instance_count
+  count   = var.instance_count > 0 ? 1 : 0
   name    = "${var.project_name}-allow-http"
   network = "default"
   allow {
